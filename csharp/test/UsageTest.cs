24cdb08c8632d4d777986b9a0cf2e515bdaf3d87
Alex Butler <alex.butler@truelayer.com>
2021-12-06 11:13:15 +0000

    C# Add `Verifier.VerifyWithJwks`, `Verifier.ExtractJku` (#26)
    * C#: Add Verifier.ExtractJku
    
    * C# Add Verifier.VerifyWithJwks

--- csharp/test/UsageTest.cs
+++ csharp/test/UsageTest.cs
@@ -2,27 +2,16 @@ using Xunit;
 using TrueLayer.Signing;
 using System;
 using FluentAssertions;
+using System.IO;
+using System.Text;
 
 namespace Tests
 {
     public class UsageTest
     {
-        // Note: Should be the same keys used in all lang tests
-        //       so static signature tests ensure cross-lang consistency.
-        internal const string PublicKey = "-----BEGIN PUBLIC KEY-----\n"
-            + "MIGbMBAGByqGSM49AgEGBSuBBAAjA4GGAAQBVIVnghUzHmCEZ3HNjDmaZMJ7UwZf\n"
-            + "av2SYcEtbDQc4uPhiEwWoYZMxzgvsz1vVGkusfTIjcXeCfDZ+xu9grRYt4kBo39z\n"
-            + "w0i0j1rau4T7Bi+thc/VZpCyuwt63mZWcRs5PlQzpL34bBSXL5L6G9XUtXn8pXwU\n"
-            + "GMhNDp5xVGbslRqTU8s=\n"
-            + "-----END PUBLIC KEY-----\n";
-        internal const string PrivateKey = "-----BEGIN EC PRIVATE KEY-----\n"
-            + "MIHcAgEBBEIAVItA/A9H8WA0rOmDO5kq774be6noZ73xWJkbmzihkhtnYJ+eCQl4\n"
-            + "G68ZFKildLuR2DElMBrNgJHY1TkL9hr7U9GgBwYFK4EEACOhgYkDgYYABAFUhWeC\n"
-            + "FTMeYIRncc2MOZpkwntTBl9q/ZJhwS1sNBzi4+GITBahhkzHOC+zPW9UaS6x9MiN\n"
-            + "xd4J8Nn7G72CtFi3iQGjf3PDSLSPWtq7hPsGL62Fz9VmkLK7C3reZlZxGzk+VDOk\n"
-            + "vfhsFJcvkvob1dS1efylfBQYyE0OnnFUZuyVGpNTyw==\n"
-            + "-----END EC PRIVATE KEY-----\n";
         internal const string Kid = "45fc75cf-5649-4134-84b3-192c2c78e990";
+        internal static string PrivateKey = File.ReadAllText(TestResourcePath("ec512-private.pem"));
+        internal static string PublicKey = File.ReadAllText(TestResourcePath("ec512-public.pem"));
 
         [Fact]
         public void SignAndVerify()
@@ -60,7 +49,7 @@ namespace Tests
                 .Sign();
 
             Verifier.VerifyWithPem(PublicKey)
-                .Method("POST") 
+                .Method("POST")
                 .Path(path)
                 .Body(body)
                 .Verify(tlSignature); // should not throw
@@ -74,7 +63,7 @@ namespace Tests
             var body = "{\"currency\":\"GBP\",\"max_amount_in_minor\":5000000}";
             var idempotency_key = "idemp-2076717c-9005-4811-a321-9e0787fa0382";
             var path = "/merchant_accounts/a61acaef-ee05-4077-92f3-25543a11bd8d/sweeping";
-            var tlSignature = "eyJhbGciOiJFUzUxMiIsImtpZCI6IjQ1ZmM3NWNmLTU2NDktNDEzNC04NGIzLTE5MmMyYzc4ZTk5MCIsInRsX3ZlcnNpb24iOiIyIiwidGxfaGVhZGVycyI6IklkZW1wb3RlbmN5LUtleSJ9..AfhpFccUCUKEmotnztM28SUYgMnzPNfDhbxXUSc-NByYc1g-rxMN6HS5g5ehiN5yOwb0WnXPXjTCuZIVqRvXIJ9WAPr0P9R68ro2rsHs5HG7IrSufePXvms75f6kfaeIfYKjQTuWAAfGPAeAQ52PNQSd5AZxkiFuCMDvsrnF5r0UQsGi";
+            var tlSignature = File.ReadAllText(TestResourcePath("tl-signature.txt")).Trim();
 
             Verifier.VerifyWithPem(PublicKey)
                 .Method("POST")
@@ -265,5 +254,41 @@ namespace Tests
 
             Verifier.ExtractKid(tlSignature).Should().Be(Kid);
         }
+
+        [Fact]
+        public void Verifier_ExtractJku()
+        {
+            var tlSignature = File.ReadAllText(TestResourcePath("webhook-signature.txt")).Trim();
+            Verifier.ExtractJku(tlSignature).Should().Be("https://webhooks.truelayer.com/.well-known/jwks");
+        }
+
+        [Fact]
+        public void Verifier_Jwks()
+        {
+            var tlSignature = File.ReadAllText(TestResourcePath("webhook-signature.txt")).Trim();
+            var jwks = File.ReadAllText(TestResourcePath("jwks.json"));
+
+            Verifier.VerifyWithJwks(jwks)
+                .Method("POST")
+                .Path("/tl-webhook")
+                .Header("x-tl-webhook-timestamp", "2021-11-29T11:42:55Z")
+                .Header("content-type", "application/json")
+                .Body("{\"event_type\":\"example\",\"event_id\":\"18b2842b-a57b-4887-a0a6-d3c7c36f1020\"}")
+                .Verify(tlSignature); // should not throw
+
+            Action verify = () => Verifier.VerifyWithJwks(jwks)
+                .Method("POST")
+                .Path("/tl-webhook")
+                .Header("x-tl-webhook-timestamp", "2021-12-02T14:18:00Z") // different
+                .Header("content-type", "application/json")
+                .Body("{\"event_type\":\"example\",\"event_id\":\"18b2842b-a57b-4887-a0a6-d3c7c36f1020\"}")
+                .Verify(tlSignature);
+
+            verify.Should().Throw<SignatureException>();
+        }
+
+        /// <summary>Return working path to /test-resources/$subpath</summary>
+        private static string TestResourcePath(string subpath)
+            => Path.Combine("../../../../../test-resources", subpath);
     }
 }
