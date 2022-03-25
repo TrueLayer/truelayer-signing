ef423437d4476f231ae8156e31d632cd362d1a14
tl-flavio-barinas <flavio.barinas@truelayer.com>
2022-03-25 09:18:12 +0000

    update comments

--- rust/examples/sign-request/src/main.rs
+++ rust/examples/sign-request/src/main.rs
@@ -7,27 +7,31 @@ const TL_BASE_URL: &str = "https://api.truelayer-sandbox.com";
 
 #[tokio::main]
 async fn main() {
-    // load env vars
+    // Read required env vars
     let kid = env::var("KID").expect("Missing env var KID");
     let access_token = env::var("ACCESS_TOKEN").expect("Missing env var ACCESS_TOKEN");
     let private_key = env::var("PRIVATE_KEY").expect("Missing env var PRIVATE_KEY");
 
-    // create idemoptency key and body
-    let idempotency_key = Uuid::new_v4().to_string();
+    // A random body string is enough for this request as `/test-signature` endpoint does not
+    // require any schema, it simply checks the signature is valid against what's received.
     let body = format!("body-{}", rand::random::<u32>());
 
-    // generate tl-signature
+    let idempotency_key = Uuid::new_v4().to_string();
+
+    // Generate tl-signature
     let tl_signature = truelayer_signing::sign_with_pem(kid.as_str(), private_key.as_bytes())
-        .method("POST")
-        .path("/test-signature")
+        .method("POST") // as we're sending a POST request
+        .path("/test-signature") // the path of our request
+        // Optional: /test-signature does not require any headers, but we may sign some anyway.
+        // All signed headers *must* be included unmodified in the request.
         .header("Idempotency-Key", idempotency_key.as_bytes())
         .header("X-Bar-Header", b"abc123")
-        .body(body.as_bytes())
+        .body(body.as_bytes()) // body of our request
         .sign()
         .unwrap();
 
-    // call `/test-signature` endpoint
     let client = reqwest::Client::new();
+    // Request body & any signed headers *must* exactly match what was used to generate the signature.
     let response = client
         .post(format!("{}/test-signature", TL_BASE_URL))
         .header("Authorization", format!("Bearer {access_token}"))
@@ -39,13 +43,13 @@ async fn main() {
         .await
         .unwrap();
 
-    // print result
-    match response.error_for_status_ref() {
-        Ok(res) => println!("{} ✓", res.status()),
-        Err(err) => println!(
-            "{}: {}",
-            err.status().unwrap(),
-            response.text().await.unwrap()
-        ),
-    }
+    let status = response.status();
+    let response_body = match status.is_success() {
+        true => String::from("✓"),
+        false => response.text().await.unwrap(),
+    };
+
+    // 204 means success
+    // 401 means either the access token is invalid, or the signature is invalid.
+    println!("{} {}", status.as_u16(), response_body);
 }
