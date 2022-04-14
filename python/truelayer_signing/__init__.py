1a620713cf1e31efc1d5ac3e090c3e387c809be9
tl-flavio-barinas <flavio.barinas@truelayer.com>
2022-04-14 16:04:03 +0100

    adds webhook example

--- python/truelayer_signing/__init__.py
+++ python/truelayer_signing/__init__.py
@@ -1,7 +1,7 @@
 # local imports
 from .sign import TlSigner
 from .utils import HttpMethod as HttpMethod
-from .verify import TlVerifier
+from .verify import TlVerifier, KeyFmt, extract_jws_header as extract_jws_header
 
 
 def sign_with_pem(kid: str, pkey: str) -> TlSigner:
@@ -16,4 +16,11 @@ def verify_with_pem(pkey: str) -> TlVerifier:
     """
     Start building a `Tl-Signature` verifier using public key pem data.
     """
-    return TlVerifier(pkey)
+    return TlVerifier(pkey, KeyFmt.PEM)
+
+
+def verify_with_jkws(jkws: str) -> TlVerifier:
+    """
+    Start building a `Tl-Signature` verifier using public key jkws data.
+    """
+    return TlVerifier(jkws, KeyFmt.JWKS)
